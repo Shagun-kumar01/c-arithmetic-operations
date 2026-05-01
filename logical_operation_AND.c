21098dc45ffe7d3f28551afdd737dbe7c1e76884
Shagun-kumar01 <lakhan4773@gmail.com>
2026-05-01 10:59:02 +0530

    Create logical_operation_AND.c

---
+++ logical_operation_AND.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a=36, b=45;
+    if (a>23 && b>42)
+        printf("Both conditions are true");
+
+    else 
+    printf(" At least one condition is false");
+    return 0;
+}

bc6e459a7577031ac68d1b56b73e96d0401c0ea7
Shagun-kumar01 <lakhan4773@gmail.com>
2026-05-04 16:22:50 +0530

    Create bitwise_not_complement.c

---
+++ bitwise_not_complement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int num, result;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    result = ~num;
+
+    printf("Bitwise NOT of %d is %d\n", num, result);
+
+    return 0;
+}

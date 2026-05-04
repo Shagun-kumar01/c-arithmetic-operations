a5a147b1513f88c02dbb1242e478bba6cbdcb507
Shagun-kumar01 <lakhan4773@gmail.com>
2026-05-04 17:04:40 +0530

    Create pre_increment.c

---
+++ pre_increment.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main()
+{
+int a;
+a = 4;
+
+printf(" Enter the first value is = %d \n", a);
+
+printf(" After increment the first value is = %d \n", ++a); // first increase then use 
+
+printf(" The final result is = %d \n", a); 
+
+return 0;
+}
+
+
+// output is --
+// Enter the first value is = 4 
+// After increment the first value is = 5 
+// The final result is = 5 

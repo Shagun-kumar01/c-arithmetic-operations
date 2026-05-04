ee424334608ffda55bd96c3ce0081aac56e9519a
Shagun-kumar01 <lakhan4773@gmail.com>
2026-05-04 17:16:07 +0530

    Create post_increment.c

---
+++ post_increment.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+int x;
+x = 5;
+
+printf(" Enter any value : %d \n", x);
+
+printf(" Increment value : %d \n", x++); // x++ means here first use,then increment 
+
+printf(" Final answer after increment : %d \n", x);
+
+return 0;
+}
+
+// output is --
+// Enter any value : 5
+// Incremetn value : 5
+// Final answer after increment : 6
